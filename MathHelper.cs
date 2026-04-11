efaeddc23a1195a3a22c65935d83783516c1a368
puspitaaa303 <puspitadash000@gmail.com>
2026-04-11 10:45:11 +0600

    Add MathHelper class with overloaded Multiply methods

---
+++ MathHelper.cs
@@ -0,0 +1,16 @@
+using System;
+class MathHelper
+{
+    public int Multiply(int a,int b)
+    {
+        return a*b;
+    }
+    public double Multiply(double a,double b)
+    {
+        return a*b;
+    }
+    public int Multiply(int a,int b,int c)
+    {
+        return a*b*c;
+    }
+}
